d0f83ac7105c46312e06f67355a1d6beda6d8ff3
AppDevsOP <testmesasoporte@gmail.com>
2024-07-28 08:13:46 -0500

    Funciona Tratamientos

--- Vista/actualizarTratamiento.php
+++ Vista/actualizarTratamiento.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Consulta de Tratamiento</title>
+  <title>MeDSyS</title>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet"> 
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link rel="stylesheet" type="text/css" href="css/stylemenu.css">
+  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
@@ -16,7 +18,7 @@
     <div class="form-horizontal">
       <form id="form1" name="form1" action="" method="POST">
         <div class="form-group">
-          <label class="col-sm-4 control-label">Nombre Tratamiento</label>
+          <label class="col-sm-4 control-label">Nombre</label>
           <input name="nombre" type="text" id="nombre" class="form-control col-sm-5" required>
         </div>
         <div class="form-group">
@@ -29,54 +31,63 @@
       <hr />
     </div>
   </div>
+
+  <script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <!-- Código de JavaScript para mostrar mensajes emergentes -->
+  <?php
+  if (isset($_GET['msj'])) {
+    if ($_GET['msj'] == 1) {
+      echo '<script type="text/javascript">
+              alert("Los Datos del Tratamiento Fueron Actualizados");
+              window.location.href="index2.php?pag=actualizarTratamiento";
+            </script>';
+    } elseif ($_GET['msj'] == 2) {
+      echo '<script type="text/javascript">
+              alert("La Información No Pudo Ser Actualizada");
+              window.location.href="index2.php";
+            </script>';
+    }
+  }
+  ?>
 </body>
-<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </html>
 
 <?php
-extract($_POST); 
+extract($_POST);
 require "../Modelo/conexionBaseDatos.php";
 require "../Modelo/tratamiento.php";
 
 if (isset($_POST['nombre'])) {
-    $objTratamiento = new Tratamiento();
-    $resultado = $objTratamiento->ConsultarTratamiento($_POST['nombre']);
-    if (isset($resultado)) {
-        if ($resultado->num_rows > 0) {
-            ?>
-            <h1 align="center">DATOS DEL TRATAMIENTO</h1>
-            <table class="table table-hover text-center mt-3">
-                <thead>
-                    <th class="text-center">Nombre</th>
-                    <th class="text-center">Duración (meses)</th>
-                    <th class="text-center">Costo</th>
-                    <th class="text-center">ID Tratamiento</th>
-                    <th class="text-center">Acción</th>
-                </thead>
-                <?php
-                while ($registro = $resultado->fetch_object()) {
-                    ?>
-                    <tr>
-                        <td><?php echo $registro->tratNombre ?></td>
-                        <td><?php echo $registro->tratDuracion ?></td>
-                        <td><?php echo $registro->tratCosto ?></td>
-                        <td><?php echo $registro->idTratamiento ?></td>
-                        <td>
-                            <a href="index2.php?pag=editarTratamiento&idTratamiento=<?php echo $registro->idTratamiento ?>">
-                                <span class="class btn btn-warning">Editar</span>
-                            </a>
-                        </td>
-                    </tr>
-                    <?php
-                }
-                ?>
-            </table>
-            <?php
-        } else {
-            echo '<div class="alert alert-danger text-center">El tratamiento no existe en la base de datos</div>';
-        }
-    }
+  $objTratamiento = new Tratamiento();
+  $resultado = $objTratamiento->consultarTratamiento($_POST['nombre']);
+  if (isset($resultado)) {
+    if ($resultado->num_rows > 0) { ?>
+
+      <h1 align="center">DATOS DEL TRATAMIENTO</h1>
+      <table class="table table-hover text-center mt-3">
+        <thead>
+          <th class="text-center">Nombre</th>
+          <th class="text-center">Duración</th>
+          <th class="text-center">Costo</th>
+          <th class="text-center">Acción</th>
+        </thead>
+        <?php while ($registro = $resultado->fetch_object()) { ?>
+          <tr>
+            <td><?php echo $registro->tratNombre ?></td>
+            <td><?php echo $registro->tratDuracion ?></td>
+            <td><?php echo $registro->tratCosto ?></td>
+            <td>
+              <a href="index2.php?pag=editarTratamiento&nombre=<?php echo $registro->tratNombre ?>">
+                <span class="class btn btn-warning">Editar</span>
+              </a>
+            </td>
+          </tr>
+        <?php } // Cerrando el ciclo while ?>
+      </table>
+    <?php } else { echo '<div class="alert alert-danger text-center">El Tratamiento No existe en la base de datos</div>'; }
+  }
 }
 ?>
--- Vista/consultarTratamiento.php
+++ Vista/consultarTratamiento.php
@@ -1,14 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>MeDSyS</title>
+  <title>Consultar Tratamiento</title>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="css/styles.css">
-  <link rel="stylesheet" type="text/css" href="css/stylemenu.css">
-  <link href="https://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet"> 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
@@ -16,10 +11,10 @@
     <h3 class="text-center pt-5">Digite el Nombre del Tratamiento a Consultar...</h3>
     <hr />
     <div class="form-horizontal">
-      <form id="form1" name="form1" action="../Controlador/validarConsultarTratamiento.php" method="POST">
+      <form id="form1" name="form1" action="" method="POST">
         <div class="form-group">
-          <label class="col-sm-4 control-label">Nombre del Tratamiento</label>
-          <input name="nombreTratamiento" type="text" id="nombreTratamiento" class="form-control col-sm-5" required>
+          <label class="col-sm-4 control-label">Nombre</label>
+          <input name="nombre" type="text" id="nombre" class="form-control col-sm-5" required>
         </div>
         <div class="form-group">
           <label class="col-sm-4 control-label"></label>
@@ -29,38 +24,35 @@
         </div>
       </form>
       <hr />
-      
-      <?php if (isset($_GET['msj'])): ?>
-        <?php if ($_GET['msj'] == 1): ?>
-          <div class="alert alert-success text-center">Tratamiento(s) encontrado(s).</div>
-        <?php elseif ($_GET['msj'] == 2): ?>
-          <div class="alert alert-danger text-center">El tratamiento no se encontró.</div>
-        <?php endif; ?>
-      <?php endif; ?>
-
-      <?php if (isset($_SESSION['resultadoTratamiento'])): ?>
-        <?php $resultado = $_SESSION['resultadoTratamiento']; ?>
-        <h1 align="center">DATOS DEL TRATAMIENTO</h1>
-        <table class="table table-hover text-center mt-3">
-          <thead>
-            <th class="text-center">Nombre Tratamiento</th>
-            <th class="text-center">Duración</th>
-            <th class="text-center">Costo</th>
-          </thead>
-          <?php while ($registro = $resultado->fetch_object()): ?>
-            <tr>
-              <td><?php echo $registro->tratNombre; ?></td>
-              <td><?php echo $registro->tratDuracion; ?></td>
-              <td><?php echo $registro->tratCosto; ?></td>
-            </tr>
-          <?php endwhile; ?>
-        </table>
-        <?php unset($_SESSION['resultadoTratamiento']); ?>
-      <?php endif; ?>
     </div>
   </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
-<script type="text/javascript" src="js/jquery-3.3.1.slim.min.js"></script>
-<script type="text/javascript" src="js/popper.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </html>
+
+<?php
+if (isset($_POST['nombre'])) {
+  require "../Modelo/conexionBaseDatos.php";
+  require "../Modelo/tratamiento.php";
+
+  $objTratamiento = new Tratamiento();
+  $resultado = $objTratamiento->consultarTratamiento($_POST['nombre']);
+  if ($resultado && $resultado->num_rows > 0) {
+    echo '<h1 class="text-center">DATOS DEL TRATAMIENTO</h1>';
+    echo '<table class="table table-hover text-center mt-3">';
+    echo '<thead><tr><th>Nombre</th><th>Duración</th><th>Costo</th></tr></thead>';
+    while ($registro = $resultado->fetch_object()) {
+      echo '<tr>';
+      echo '<td>' . $registro->tratNombre . '</td>';
+      echo '<td>' . $registro->tratDuracion . '</td>';
+      echo '<td>' . $registro->tratCosto . '</td>';
+      echo '</tr>';
+    }
+    echo '</table>';
+  } else {
+    echo '<div class="alert alert-danger text-center">El tratamiento no existe en la base de datos</div>';
+  }
+}
+?>
