153b4190dda9ddfa6e9e96beeee6622ac6717698
AppDevsOP <jhondacelis@gmail.com>
2024-07-30 09:12:23 -0500

    Bug de atendido y asignado solucionado

--- Controlador/validarActualizarCita.php
+++ Controlador/validarActualizarCita.php
@@ -5,13 +5,19 @@ require "../Modelo/cita.php";
 extract($_POST);
 
 if (isset($_POST['idCita'])) {
-    $objCita = new Cita();
-    $resultado = $objCita->actualizarCita($_POST['idCita'], $_POST['fecha'], $_POST['hora'], $_POST['paciente'], $_POST['medico'], $_POST['consultorio'], $_POST['estado'], $_POST['observaciones']);
+    $estadoValido = ['asignado', 'atendido'];
+    if (in_array($_POST['estado'], $estadoValido)) {
+        $objCita = new Cita();
+        $resultado = $objCita->actualizarCita($_POST['idCita'], $_POST['fecha'], $_POST['hora'], $_POST['paciente'], $_POST['medico'], $_POST['consultorio'], $_POST['estado'], $_POST['observaciones']);
 
-    if ($resultado) {
-        header("Location: ../Vista/index2.php?pag=actualizarCita&msj=1");
+        if ($resultado) {
+            header("Location: ../Vista/index2.php?pag=actualizarCita&msj=1");
+        } else {
+            header("Location: ../Vista/index2.php?pag=actualizarCita&msj=2");
+        }
     } else {
-        header("Location: ../Vista/index2.php?pag=actualizarCita&msj=2");
+        // Si el estado no es válido, redirige con un mensaje de error
+        header("Location: ../Vista/index2.php?pag=actualizarCita&msj=3");
     }
 }
 ?>
